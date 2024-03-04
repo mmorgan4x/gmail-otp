a1f754ecb5259d900c1c4b867e5253d521553811
Matt Morgan <matt.morgan@cmgfi.com>
2024-03-04 15:51:07 -0500

    style updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Gmail, Oauth2 } from './types';
 export class AppComponent {
 
   loading: boolean = false;
-  codes: string[]
+  codes: any[]
   email: string
   userInfo?: Oauth2.UserInfo;
   messages?: Gmail.Message[] = []
@@ -31,12 +31,35 @@ export class AppComponent {
     this.messages = await Promise.all(emailsIds.map(t => this.api.getEmailById(t)));
     this.loading = false;
     this.cdr.detectChanges();
+    console.log(this.messages)
 
-    //       const otpMatch = email.snippet.match(/\b\d{6}\b/);
-    //       if (otpMatch) {
-    //         otpCodes.push(otpMatch[0]);
-    //       }
-    //     });
-
+    this.codes = this.messages.map(msg => {
+      return {
+        msg: msg,
+        unread: msg.labelIds.includes('UNREAD'),
+        subject: this.getHeader(msg, 'Subject'),
+        snippet: this.replaceHtmlEntities(msg.snippet.trim()),
+        from: this.parseEmail(this.getHeader(msg, 'From')),
+        to: this.getHeader(msg, 'To'),
+        date: new Date(+msg.internalDate),
+        code: msg.snippet.match(/\b\d{6}\b/)?.at(0)
+      }
+    })
   }
+
+  getHeader(msg: Gmail.Message, name: string) {
+    return msg?.payload?.headers?.find(t => t.name == name)?.value;
+  }
+
+  parseEmail(email: string) {
+    const match = email.match(/"?(.*?)"?\s<([^>]+)>/);
+    return { display: match?.at(1), value: match?.at(2) };
+  }
+
+  replaceHtmlEntities(text: string) {
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = text;
+    return tempElement.textContent || tempElement.innerText;
+  }
+
 }
